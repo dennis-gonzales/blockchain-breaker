d1b7d47e06899579de3ee65f3c71dfbfeb056f93
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2020-11-21 20:41:36 +0800

    get player data

---
+++ Assets/CHI/Ethereum/Request/PlayerDataRequest.cs
@@ -0,0 +1,43 @@
+﻿using Ethereum;
+using Nethereum.Web3;
+using UnityEngine;
+using Nethereum.Web3.Accounts;
+using Ethereum.Wrapper;
+
+namespace Blockchain.Request
+{
+    public class PlayerDataRequest : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetPlayerData();
+        }
+        public async void GetPlayerData()
+        {
+            Debug.Log("GetPlayerData()");
+
+            var account = new Account(Environment.Account2PK);
+            var web3 = new Web3(account, Environment.InfuraKey);
+            var contractHandler = web3.Eth.GetContractHandler(Environment.contractAddress);
+
+            // var playerDataHandler = web3.Eth.GetContractQueryHandler<PlayerData.GetPlayerDataFunction>();
+            // var playerDataOutput = await playerDataHandler.QueryDeserializingToObjectAsync<PlayerData.GetPlayerDataOutputDTO>
+            // (
+            //     new PlayerData.GetPlayerDataFunction(),
+            //     Environment.contractAddress
+            // );
+
+            var playerDataOutput = await contractHandler.QueryDeserializingToObjectAsync
+            <PlayerData.GetPlayerDataFunction, PlayerData.GetPlayerDataOutputDTO>
+            (
+                new PlayerData.GetPlayerDataFunction()
+            );
+
+
+            Debug.Log(playerDataOutput);
+            Debug.Log(playerDataOutput.HighScore);
+            Debug.Log(playerDataOutput.CurrentLevel);
+            Debug.Log(playerDataOutput.LevelsCleared);
+        }
+    }
+}
---
+++ Assets/CHI/Ethereum/Wrapper/PlayerData.cs
@@ -0,0 +1,41 @@
+﻿using System.Numerics;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+
+namespace Ethereum.Wrapper
+{
+    public static class PlayerData
+    {
+
+        [Function("getPlayerData", typeof(GetPlayerDataOutputDTO))]
+        public class GetPlayerDataFunction : FunctionMessage
+        {
+
+        }
+
+        [FunctionOutput]
+        public class GetPlayerDataOutputDTO : IFunctionOutputDTO
+        {
+            [Parameter("bool", "isActive", 1)]
+            public bool IsActive { get; set; }
+
+
+            [Parameter("uint256", "highscore", 2)]
+            public BigInteger HighScore { get; set; }
+
+
+            [Parameter("uint256", "levelsCleared", 3)]
+            public BigInteger LevelsCleared { get; set; }
+
+
+            [Parameter("uint256", "currentLevel", 4)]
+            public BigInteger CurrentLevel { get; set; }
+            
+
+            public override string ToString()
+            {
+                return $"Is Active: {IsActive}";
+            }
+        }
+    }
+}
